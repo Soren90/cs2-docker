3cd910797c1d1772840cd6eb5949d676d464e83e
soren90 <nicklassr@gmail.com>
2023-09-30 20:06:27 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# CS2 dockerized
+
+This project is still in early development. But with this docker image you should be able to run a cs2 server in a pretty simple manner. Pull requests are open and I'll be happy to implement improvements to the project
+
+## How to use
+
+Install docker:
+```bash
+curl -fsSL https://get.docker.com/ -o install-docker.sh | sh install-docker.sh
+```
+  
+Add your steam username and password to the docker-compose.yml (Ideally a seperate account that doesn't have steam guard if possible. Not tested)
+  
+start the server:
+```bash
+docker-compose up -d
+```
+
+If you are using an account with steamguard enabled, you need to attach the container and put in your code
+```bash
+docker attach cs2-ds
+```
+
+If you need to modify the server files, you will find the server files here: /var/lib/docker/volumes/cs2-data/_data/ 
+
+## TODO
+
+- Fix RCON
+- Get rid of host networking and automatically fetch the container IP.
+- Add sourcemod/metamod when ready
+- Put username/password into secret
+- Improve this document
+
+## Credits
+
+This is heavily inspired by [kaimallea/csgo](https://github.com/kaimallea/csgo)
+Remote RCON with this tool seems like a good addition for now [shobhit-pathak/cs2-rcon-panel](https://github.com/shobhit-pathak/cs2-rcon-panel)
---
+++ entrypoint.sh
@@ -0,0 +1,71 @@
+sleep 1
+# Mandatory variables check
+: "${STEAM_DIR:?'ERROR: STEAM_DIR IS NOT SET!'}"
+: "${STEAMCMD_DIR:?'ERROR: STEAMCMD_DIR IS NOT SET!'}"
+: "${CS2_DIR:?'ERROR: CSGO_DIR IS NOT SET!'}"
+: "${USER:?'ERROR: USER IS NOT SET!'}"
+: "${PASSWORD:?'ERROR: PASSWORD IS NOT SET!'}"
+
+# Set defaults
+export SERVER_HOSTNAME="${SERVER_HOSTNAME:-cs2server}"
+export PORT="${PORT:-27015}"
+export GAME_TYPE="${GAME_TYPE:-0}"
+export GAME_MODE="${GAME_MODE:-1}"
+export MAP="${MAP:-de_dust2}"
+export MAPGROUP="${MAPGROUP:-mg_active}"
+export MAXPLAYERS="${MAXPLAYERS:-12}"
+export IP="${IP:-0.0.0.0}"
+
+# Copy competitive configs
+mkdir -p ${CS2_DIR}/game/csgo/cfg
+cp /mnt/cfg/* ${CS2_DIR}/game/csgo/cfg
+
+if [ ! -s "$CS2_DIR/game/csgo/cfg/autoexec.cfg" ]; then
+cat << AUTOEXECCFG > "$CS2_DIR/game/csgo/cfg/autoexec.cfg"
+log on
+hostname "$SERVER_HOSTNAME"
+rcon_password "$RCON_PASSWORD"
+sv_password "$SERVER_PASSWORD"
+sv_cheats 0
+exec banned_user.cfg
+exec banned_ip.cfg
+AUTOEXECCFG
+
+else
+sed -i "s/^hostname.*/hostname \"$SERVER_HOSTNAME\"/" $CS2_DIR/game/csgo/cfg/autoexec.cfg
+sed -i "s/^rcon_password.*/rcon_password \"$RCON_PASSWORD\"/" $CS2_DIR/game/csgo/cfg/autoexec.cfg
+sed -i "s/^sv_password.*/sv_password \"$SERVER_PASSWORD\"/" $CS2_DIR/game/csgo/cfg/autoexec.cfg
+
+fi
+
+# Create dynamic server config
+if [ ! -s "$CS2_DIR/game/csgo/cfg/server.cfg" ]; then
+cat << SERVERCFG > "$CS2_DIR/game/csgo/cfg/server.cfg"
+tv_delaymapchange 1
+tv_delay 30
+tv_deltacache 2
+tv_dispatchmode 1
+tv_maxclients 10
+tv_maxrate 0
+tv_overridemaster 0
+tv_relayvoice 1
+tv_snapshotrate 64
+tv_timeout 60
+tv_transmitall 1
+writeid
+writeip
+SERVERCFG
+fi
+
+# Install/update game
+${STEAMCMD_DIR}/steamcmd.sh +login $USER $PASSWORD +force_install_dir ${CS2_DIR} +app_update 730 +quit
+
+# hacky error fix
+mkdir -p ${STEAM_DIR}/.steam/sdk32
+mkdir -p ${STEAM_DIR}/.steam/sdk64
+
+cp ${STEAMCMD_DIR}/linux32/steamclient.so ${STEAM_DIR}/.steam/sdk32/steamclient.so
+cp ${STEAMCMD_DIR}/linux64/steamclient.so ${STEAM_DIR}/.steam/sdk64/steamclient.so
+
+# Start gameserver
+${CS2_DIR}/game/cs2.sh +ip ${IP} -port ${PORT} -dedicated -game csgo -console -usercon +map ${MAP} +gametype ${GAME_TYPE} +gamemode ${GAME_MODE}
